c4f82e2c3ea5cbec540fb98c6de804c30aee8184
swathi <swathi@gmail.com>
2023-12-29 12:57:11 +0000

    feature/ftp-port commit

--- main.tf
+++ main.tf
@@ -37,6 +37,14 @@ resource "aws_security_group" "frontend" {
     cidr_blocks      = ["0.0.0.0/0"]
     ipv6_cidr_blocks = ["::/0"]
   }
+  ingress {
+    from_port        = 21
+    to_port          = 21
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
   egress {
     from_port        = 0
     to_port          = 0
